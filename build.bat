c65018059a2cdb3c70953e0b32fa42c1c0a47990
amozgachev <amozgachev@iknow.travel>
2015-11-09 15:56:42 +0300

    Implement unicode support

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@ echo off
 cls
 set path=C:\mingw\bin;%path%
 windres res.rc -O coff -o res.res
-gcc -Wall main.c res.res -o main.exe -fomit-frame-pointer -funswitch-loops -O3 -Os -mwindows
+gcc -Wall main.c res.res -o main.exe -fomit-frame-pointer -funswitch-loops -O3 -Os -mwindows -DUNICODE -lShlwapi -lShell32
 strip main.exe
--- main.c
+++ main.c
@@ -1,90 +1,90 @@
 #include <stdio.h>
 #include <windows.h>
+#include <io.h>
+#include <wchar.h>
+#include <shellapi.h>
+#include "shlwapi.h"
+
+#define _O_U16TEXT  0x20000
 
 int main(int argc,char**argv)
 {
-	char res[4096];
-	char res2[4096];
-	char command[4096];
-	char mshta[MAX_PATH];
-	char curdir[MAX_PATH];
-	char curdirBin[MAX_PATH];
-	int i;
-	int last;
-	char s[MAX_PATH];
-	
-	SetEnvironmentVariable("SEE_MASK_NOZONECHECKS","1");
-	
+	WCHAR res[4096];
+	WCHAR res2[4096];
+	WCHAR command[4096];
+	WCHAR mshta[MAX_PATH];
+	WCHAR curdir[MAX_PATH];
+	WCHAR curdirBin[MAX_PATH];
+	WCHAR s[MAX_PATH];
+
+	_setmode(_fileno(stdout), _O_U16TEXT);
+
+	int nargs;
+	WCHAR **arglist = CommandLineToArgvW(GetCommandLineW(), &nargs);
+
+	SetEnvironmentVariable(L"SEE_MASK_NOZONECHECKS",L"1");
+
 	//Путь до mshta.exe
 	GetWindowsDirectory(mshta, MAX_PATH);
-	sprintf(mshta,"%s\\System32\\mshta.exe",mshta);
-	printf("mshtaPath: %s\n", mshta);
-	
-	
+	wsprintf(mshta,L"%s\\System32\\mshta.exe",mshta);
+	wprintf(L"mshtaPath: %s\n", mshta);
+
+
 	//Полный путь до нашего EXE
-	if (GetModuleFileName(NULL, s, sizeof(s) / sizeof(*s)) > 0)
-	printf("GetModuleFileName=%s\n", s);
-	
-	
-	//Находим последнее вхождение символа \ чтобы обрезать имя EXE
-	last=sizeof(s);
-	for (i=0; s[i]!='\0'; i++){
-		if (s[i]=='\\'){ last = i; }
-	}
-	//printf("indexOf: %d\n", last);
-	
-	
+	GetModuleFileNameW(GetModuleHandleW(NULL), s, MAX_PATH);
+	wprintf(L"GetModuleFileName=%s\n", s);
+
 	//Обрезаем имя EXE, чтобы получить именно текущую директорию
-	strncpy(curdir,s,last);
-	printf("curdir=%s\n", curdir);
-	
-	
-	
+	PathRemoveFileSpec(s);
+	wcscpy(curdir,s);
+	wprintf(L"curdir=%s\n", curdir);
+
+
+
 	//Собираем все параметры полученные через командную строку
-	strcpy(command,"");
+	wcscpy(command,L"");
 	if (argc>1){
 		int i;
 		for(i=1; i<argc; ++i) {
-			strcat(command," ");
-			strcat(command,argv[i]);
+			wcscat(command,L" ");
+			wcscat(command,arglist[i]);
 		}
 	}
-	puts(command);
+	_putws(command);
 
-	
-	
+
+
 	//Устанавливаем текущую директорию
-	sprintf(curdirBin,"%s\\bin",curdir);
+	wsprintf(curdirBin,L"%s\\bin",curdir);
 	SetCurrentDirectory(curdirBin);
-	printf("curdirBin: %s\n",curdirBin);
-	
+	wprintf(L"curdirBin: %s\n",curdirBin);
+
 	//Собираем параметры, которые будем передавать в mshta.exe
-	sprintf(res,"\"%s\\Tools\\run.hta\"%s",curdirBin,command);
-	
+	wsprintf(res,L"\"%s\\Tools\\run.hta\"%s",curdirBin,command);
+
 	//Если запускаем не через SMB, а локально
 	if(!(curdirBin[0]=='\\'&&curdirBin[1]=='\\'))
 	{
 		//Запускаем HTA-приложение
-		ShellExecute(0,"open",mshta,res,0,SW_SHOWNORMAL);
-		printf("Executed: %s %s\n",mshta,res);
+		ShellExecute(0,L"open",mshta,res,0,SW_SHOWNORMAL);
+		wprintf(L"Executed: %s %s\n",mshta,res);
 		return 0;
 	}
-	
-	
+
+
 	//Подключаем сетевой диск
 	SetCurrentDirectory(curdir);
-	system("NET USE \"Z:\" /DELETE /YES");
-	sprintf(res2,"SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION & FOR /F \"tokens=2\" \%\%D IN ('NET USE * \"%s\" /PERSISTENT:NO^|FINDSTR /I 'unidad') DO SET DRIVELETTER=\%\%D",curdir);
-	printf("NetUse: %s\n",res2);
-	system(res2);
-	
-	
+	_wsystem(L"NET USE \"Z:\" /DELETE /YES");
+	wsprintf(res2,L"SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION & FOR /F \"tokens=2\" \%\%D IN ('NET USE * \"%s\" /PERSISTENT:NO^|FINDSTR /I 'unidad') DO SET DRIVELETTER=\%\%D",curdir);
+	wprintf(L"NetUse: %s\n",res2);
+	_wsystem(res2);
+
+
 	//Запускаем HTA-приложение
-	SetCurrentDirectory("Z:\\bin\\");
-	ShellExecute(0,"open",mshta,res,0,SW_SHOWNORMAL);
-	printf("Executed: %s %s\n",mshta,res);
+	SetCurrentDirectory(L"Z:\\bin\\");
+	ShellExecute(0,L"open",mshta,res,0,SW_SHOWNORMAL);
+	wprintf(L"Executed: %s %s\n",mshta,res);
 	return 0;
 }
-
 
 
